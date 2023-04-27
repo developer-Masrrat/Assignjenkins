1333b4411b846b353e355a1f456aeedd36d24a48
Masrrat Noor <84374876+developer-Masrrat@users.noreply.github.com>
2023-04-27 16:52:47 +0530

    Add files via upload

---
+++ src/main/java/com/nagarro/assignForJenkins/AssignForJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.assignForJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignForJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignForJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarro/assignForJenkins/AssignForJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.assignForJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignForJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
